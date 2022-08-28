5aa14b9574fcd625999e93f02a00d7ad101423c3
Julek <90630830+Jobrzut@users.noreply.github.com>
2022-08-28 14:33:22 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-const countdown = () => {
-var today = new Date();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-console.log(time);
 
-document.querySelector(".godzina").innerText = time;
+const countdown = () => {
+    // Obecny czas
+    var today = new Date();
+    var godzina = today.getHours().toString().padStart(2, 0) + ":";
+    var minuta = today.getMinutes().toString().padStart(2, 0) + ":";
+    var sekunda = today.getSeconds().toString().padStart(2, 0);
+    
+    // Zamiana
+    document.querySelector(".godzina").innerText = godzina;
+    document.querySelector(".minuta").innerText =  minuta;
+    document.querySelector(".sekunda").innerText = sekunda;
 };
+
 setInterval(countdown, 1000);
